dad50a6f17c087a630ac0a96988de2b9a2d93ac7
ianband <ian.r.band@gmail.com>
2022-04-26 13:54:54 -0700

    Add object distance functionality

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ document.body.style.padding = 0;
 const gui = new dat.GUI();
 
 const debugVars = {
-    zoom: 1,
-    objectDistance: 1,
+    zoom: 0.1,
+    objectDistance: 0,
     turbulance: 1,
 }
+
+gui.add(debugVars, "zoom", 0, 1);
+gui.add(debugVars, "objectDistance", -8.5, 0.7);
+gui.add(debugVars, "turbulance", 0, 1);
 
 // camera
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
@@ -36,9 +40,16 @@ document.body.appendChild( renderer.domElement );
 
 function animation( time ) {
 
+    
+
 	mesh.rotation.x = time / 2000;
 	mesh.rotation.y = time / 1000;
 
+    mesh.position.z = debugVars.objectDistance;
+
 	renderer.render( scene, camera );
+
+    // Call tick again on the next frame
+    //window.requestAnimationFrame(tick);
 
 }
