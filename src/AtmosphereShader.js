dc8c7ae1d7e6b7ae8ecb871699c4cbe32007cba7
ianband <ian.r.band@gmail.com>
2022-04-28 17:49:37 -0700

    Add Atmosphere Pass & uniform controls

--- src/AtmosphereShader.js
+++ src/AtmosphereShader.js
@@ -7,9 +7,10 @@ const AtmosphereShader = {
     },
     uniforms: { /* these are essentially the arguments supplied to the shader */
         'time': {value: 0},
+        'time_dialation': {value: 0.1},
         'size': {value: 0.4}, // size of the distortion texture, smaller = more zoomed in
         'strength': {value: 0.02}, // distance moved by distortion
-        'show_result': {value: 0.8}, // opacity of distortion textures (inverted, 0 = 100% opacity)
+        'show_result': {value: 1.0}, // opacity of distortion textures (inverted, 0 = 100% opacity)
         // https://threejs.org/docs/#api/en/loaders/TextureLoader
         'distort_tex': {value: new THREE.TextureLoader().load(DistortionMap)}, //may have to import texture via js 'import'
         'tDiffuse': { value: null }, // honestly idek
@@ -44,19 +45,20 @@ const AtmosphereShader = {
 		varying vec2 vUv;
 
         uniform float time;
+        uniform float time_dialation;
         uniform float size;			// should be turned into a constant once you're happy with the setting
         uniform float strength;		// should be turned into a constant once you're happy with the setting
 
         uniform float show_result;	// debug only. remove this and related lines inside the objects events
 
         // main idea: make strength dependant on distance from camera
-        
+
 		void main() {
 
             // grab distortion off the distortion texture
             vec2 distort;
-            distort.x = texture2D( distort_tex, fract(vUv * size		+ vec2(0.0, time))).r		* strength;
-            distort.y = texture2D( distort_tex, fract(vUv * size * 3.4	+ vec2(0.0, time * 1.6))).g	* strength * 1.3;
+            distort.x = texture2D( distort_tex, fract(vUv * size		+ vec2(0.0, time * 0.1 * time_dialation))).r		* strength;
+            distort.y = texture2D( distort_tex, fract(vUv * size * 3.4	+ vec2(0.0, time * 1.6 * time_dialation))).g	* strength * 1.3;
 
             //// grab the base colour at the distorted texture coordinate:
 			gl_FragColor = texture2D( tDiffuse, vUv + distort);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import * as dat from "lil-gui";
 
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
-//import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'; 
-import { AtmosphereShader } from './AtmosphereShader.js'
+import { AtmospherePass } from './AtmospherePass.js';
+// import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+// import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'; 
+// import { AtmosphereShader } from './AtmosphereShader.js'
+
 
 
 // remove padding
@@ -18,10 +20,21 @@ const gui = new dat.GUI();
 const debugVars = {
     fov: 70,
     objectDistance: 0,
+    SH_time_dialation: 0.1,
+    SH_size: 0.4,
+    SH_strength: 0.02,
+    SH_show_result: 1.0
 }
 
 gui.add(debugVars, "fov", 10, 150);
 gui.add(debugVars, "objectDistance", -8.5, 0.7);
+gui.add(debugVars, "SH_time_dialation", 0, 2);
+gui.add(debugVars, "SH_size", 0, 2);
+gui.add(debugVars, "SH_strength", 0, 0.15);
+gui.add(debugVars, "SH_show_result", 0, 1);
+
+
+
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-= camera =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 const camera = new THREE.PerspectiveCamera( debugVars.fov, window.innerWidth / window.innerHeight, 0.01, 10 );
@@ -43,7 +56,8 @@ document.body.appendChild( renderer.domElement );
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-= post processing =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 const postprocessing = {};
 const renderPass = new RenderPass( scene, camera );
-const atmospherePass = new ShaderPass(AtmosphereShader);
+//const atmospherePass = new ShaderPass(AtmosphereShader); 
+const atmospherePass = new AtmospherePass(); 
 const composer = new EffectComposer( renderer );
 
 composer.addPass( renderPass );
@@ -60,12 +74,19 @@ const tick = () => {
     const deltaTime = elapsedTime - previousTime;
     previousTime = elapsedTime;
 
-    // update
+    // update mesh & scene
     mesh.rotation.x += deltaTime / 2;
 	mesh.rotation.y += deltaTime / 3;
 
     mesh.position.z = debugVars.objectDistance;
     camera.setFocalLength(debugVars.fov);
+
+    // update shader uniforms
+    atmospherePass.updateUniform('time', elapsedTime);
+    atmospherePass.updateUniform('time_dialation', debugVars.SH_time_dialation);
+    atmospherePass.updateUniform('size', debugVars.SH_size);
+    atmospherePass.updateUniform('strength', debugVars.SH_strength);
+    atmospherePass.updateUniform('show_result', debugVars.SH_show_result);
 
 	
     // render scene
